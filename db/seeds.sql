b078699a1105ddcdc920adddc4ddfea389afc169
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-24 18:47:17 -0700

    all of the code from the last repo

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green Chilie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', FALSE);
